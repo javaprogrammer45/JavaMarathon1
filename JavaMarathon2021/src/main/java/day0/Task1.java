6beef6e69ccf0db29594edd77a9d568aba0a6680
frontender43 <frontender43@gmail.com>
2024-02-26 12:48:41 +0300

    Initial commit

---
+++ JavaMarathon2021/src/main/java/day0/Task1.java
@@ -0,0 +1,13 @@
+package day0;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String str = "JAVA";
+        int count = 0;
+        while (count < 10) {
+            System.out.print("JAVA  ");
+            count++;
+        }
+    }
+
+}

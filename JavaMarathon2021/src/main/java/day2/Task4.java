dda96c0a3a69a9de4c624ccab6a55d01301b1a18
javaprogrammer45 <natureandpeople@gmail.com>
2024-03-02 12:33:18 +0300

    Alles wird good!!!

--- JavaMarathon2021/src/main/java/day2/Task4.java
+++ JavaMarathon2021/src/main/java/day2/Task4.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+
+        if (x >= 5) {
+            double y = (Math.pow(x, 2) - 10) / (x + 7);
+            System.out.println(y);
+        } else if (x > -3 && x < 5) {
+            double y = (x + 3) * (Math.pow(x, 2) - 2);
+            System.out.println(y);
+        } else {
+            System.out.println(420);
+        }
+
 
     }
 }
+

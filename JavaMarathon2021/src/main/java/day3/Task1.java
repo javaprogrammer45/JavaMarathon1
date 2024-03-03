1c7f684298099b33ca73b5d309741bd0aa4a5f2b
javaprogrammer45 <natureandpeople@gmail.com>
2024-03-03 16:13:40 +0300

    Alles wird good!!!

--- JavaMarathon2021/src/main/java/day3/Task1.java
+++ JavaMarathon2021/src/main/java/day3/Task1.java
@@ -1,7 +1,45 @@
 package day3;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.util.Objects;
+import java.util.Scanner;
 
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String city = scanner.nextLine();
+            if (city.equals("Stop")) {
+                break;
+            }
+
+            switch (city) {
+
+                case "Moscow":
+                case "Vladivostok":
+                case "Rostov":
+                    System.out.println("Russia");
+                    break;
+                case "Rome":
+                case "Milan":
+                case "Turin":
+                    System.out.println("Italy");
+                    break;
+                case "Liverpool":
+                case "Manchester":
+                case "London":
+                    System.out.println("England");
+                    break;
+                case "Berlin":
+                case "Munchen":
+                case "Koln":
+                    System.out.println("Germany");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+            }
+        }
     }
 }
+
--- JavaMarathon2021/src/main/java/day3/Task3.java
+++ JavaMarathon2021/src/main/java/day3/Task3.java
@@ -1,7 +1,26 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+
+        while (i <= 4) {
+            double dividend = scanner.nextDouble();
+            double divider = scanner.nextDouble();
+            i++;
+            if (divider == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(dividend / divider);
+        }
+
 
     }
+
+
 }
+

e2481b58a7dde534166bde2a712c1ff0c5b0b412
javaprogrammer45 <natureandpeople@gmail.com>
2024-03-10 14:34:03 +0300

    edited day2/Task4

--- JavaMarathon2021/src/main/java/day2/Task4.java
+++ JavaMarathon2021/src/main/java/day2/Task4.java
@@ -6,18 +6,17 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double x = scanner.nextDouble();
+        double y;
 
         if (x >= 5) {
-            double y = (Math.pow(x, 2) - 10) / (x + 7);
-            System.out.println(y);
+            y = (Math.pow(x, 2) - 10) / (x + 7);
         } else if (x > -3 && x < 5) {
-            double y = (x + 3) * (Math.pow(x, 2) - 2);
-            System.out.println(y);
+            y = (x + 3) * (Math.pow(x, 2) - 2);
         } else {
-            System.out.println(420);
+            y = 420;
         }
 
-
+        System.out.println(y);
     }
 }
 

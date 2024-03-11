21f6626cd13f5dcff3a51e6d89fa634494160650
javaprogrammer45 <natureandpeople@gmail.com>
2024-03-11 20:26:51 +0300

    Everything will be fine!

---
+++ JavaMarathon2021/src/main/java/day8/Airplane.java
@@ -0,0 +1,50 @@
+package day8;
+
+public class Airplane {
+    private String manufacturer;
+    private int year;
+    private float length;
+    private float weight;
+    private int fuel;
+
+
+    Airplane(String manufacturer, int year, float length, float weight) {
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        info();
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public float getFuel() {
+        return fuel;
+    }
+
+    public void info() {
+        System.out.println("Изготовитель: " + manufacturer + "," + "год выпуска: " + year + "," + "длина: " + length + "," + "вес: " + weight + ",");
+    }
+
+    public void fillUp(int n) {
+        fuel = fuel + n;
+    }
+}
--- JavaMarathon2021/src/main/java/day8/Task1.java
+++ JavaMarathon2021/src/main/java/day8/Task1.java
@@ -1,7 +1,28 @@
 package day8;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        String str = new String();
+        long startTime1 = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            String s = str.concat(String.valueOf(i));
+            //System.out.print(s + " ");
+        }
+        long stopTime1 = System.currentTimeMillis();
+        System.out.println("Длительность работы 1, в мс.: " + (stopTime1 - startTime1));
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+        long startTime2 = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            Integer n = i;
+            StringBuilder stringBuilder1 = stringBuilder.append(n);
+            //System.out.print(stringBuilder1 + " ");
+        }
+        long stopTime2 = System.currentTimeMillis();
+        System.out.println("Длительность работы 2, в мс.: " + (stopTime2 - startTime2));
     }
 }

6ff8f347c84f7bc806107091a5fb2dec4e1ce159
javaprogrammer45 <natureandpeople@gmail.com>
2024-03-16 17:24:04 +0300

    Everything will be fine!!!

--- JavaMarathon2021/src/main/java/day12/Task1.java
+++ JavaMarathon2021/src/main/java/day12/Task1.java
@@ -1,7 +1,29 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+        String a = "Audi";
+        String b = "BMW";
+        String c = "Mercedes";
+        String d = "Mitsubishi";
+        String f = "WV";
+        String n = "Renault";
 
+        List<String> cars = new ArrayList<>();
+
+        cars.add(a);
+        cars.add(b);
+        cars.add(c);
+        cars.add(d);
+        cars.add(f);
+
+
+        cars.add(cars.size() / 2, n);
+        cars.remove(0);
+        System.out.println(cars);
     }
+
 }
--- JavaMarathon2021/src/main/java/day12/Task2.java
+++ JavaMarathon2021/src/main/java/day12/Task2.java
@@ -1,7 +1,21 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task2 {
     public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbersFill(0, 30, numbers);
+        numbersFill(300, 350, numbers);
+        System.out.println(numbers);
+    }
+
+    public static void numbersFill(int l, int k, List<Integer> list) {
+        for (int i = l; i <= k; i++) {
+            list.add((Integer) i);
+        }
 
     }
 }

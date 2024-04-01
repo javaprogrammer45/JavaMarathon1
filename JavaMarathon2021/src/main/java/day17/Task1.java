6553aaf32e12f17996c8ceabf3913bbec14bd381
javaprogrammer45 <natureandpeople@gmail.com>
2024-04-01 21:02:12 +0300

    day17

--- JavaMarathon2021/src/main/java/day17/Task1.java
+++ JavaMarathon2021/src/main/java/day17/Task1.java
@@ -1,7 +1,26 @@
 package day17;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        ChessPiece[] chessPiece = ChessPiece.values();
+        ChessPiece[] chessPiece1 = new ChessPiece[8];
+
+        for (int i = 0; i < chessPiece1.length; i++) {
+            if ((i < (chessPiece1.length) / 2)) {
+                chessPiece1[i] = chessPiece[ChessPiece.PAWN_WHITE.ordinal()];
+            } else {
+                chessPiece1[i] = chessPiece[ChessPiece.ROOK_BLACK.ordinal()];
+            }
+        }
+
+        for (ChessPiece d : chessPiece1) {
+            System.out.print(d.getStringDesignation());
+        }
+
     }
+
+
 }
